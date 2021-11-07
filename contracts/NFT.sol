3a7163c12cb734e0c0aa5ec9bbe022ba78065152
jp93arg <jpcarrafancq@gmail.com>
2021-11-07 19:58:27 -0300

    add NFT contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.counter;
+    Counters.counter private _tokenIds;
+    address contractAddress;
+
+    constructor (address marketplaceAddress) ERC721("Marketplace Tokens", "MEET") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+
+        return newTokenId;
+    }
+}
