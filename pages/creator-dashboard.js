cbff0de8653ebed401f07e0d8d13b4158fceb0cb
jp93arg <jpcarrafancq@gmail.com>
2021-11-16 15:58:28 -0300

    add auctions to creator dashboard

--- pages/creator-dashboard.js
+++ pages/creator-dashboard.js
@@ -10,13 +10,14 @@ import NFTMarket from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
 export default function ListMyAssets() {
     const [nfts, setNfts] = useState([]);
+    const [auctions, setAuctions] = useState([]);
     const [soldNfts, setSoldNfts] = useState([]);
     const [loadingState, setLoadingState] = useState('not-loaded');
     useEffect(() => {
-        loadNFTs();
+        load();
     }, []);
 
-    async function loadNFTs() {
+    async function load() {
         const web3modal = new Web3Modal();
         const connection = await web3modal.connect();
         const provider = new ethers.providers.Web3Provider(connection);
@@ -24,6 +25,63 @@ export default function ListMyAssets() {
 
         const marketContract = new ethers.Contract(nftMarketAddress, NFTMarket.abi, signer);
         const tokenContract = new ethers.Contract(nftAddress, NFT.abi, signer);
+
+        const auctionsPromise = loadAuctions(marketContract, tokenContract, signer);
+        const nftsPromise = loadNFTs(marketContract, tokenContract, signer);
+        await Promise.all([auctionsPromise, nftsPromise]);
+
+        setLoadingState('loaded');
+    }
+
+    async function loadAuctions(marketContract, tokenContract, signer) {
+        // const web3modal = new Web3Modal();
+        // const connection = await web3modal.connect();
+        // const provider = new ethers.providers.Web3Provider(connection);
+        // const signer = provider.getSigner();
+
+        // const marketContract = new ethers.Contract(nftMarketAddress, NFTMarket.abi, signer);
+        // const tokenContract = new ethers.Contract(nftAddress, NFT.abi, signer);
+        const data = await marketContract.getMyAuctions();
+        console.log(`data is ${JSON.stringify(data)}`);
+
+        const items = await Promise.all(data.map(async (i) => {
+            const tokenUri = await tokenContract.tokenURI(i.itemId);
+            const metadata = await axios.get(tokenUri);
+            let startingPrice = ethers.utils.formatUnits(i.startingPrice.toString(), "ether");
+            let highestBid = ethers.utils.formatUnits(i.highestBid.toString(), "ether");
+
+            return {
+                id: i.itemId,
+                image: metadata && metadata.data && metadata.data.image,
+                name: metadata && metadata.data && metadata.data.name,
+                startingPrice,
+                highestBid,
+                auctionId: i.auctionId,
+                auctionEnd: i.auctionEnd,
+                claimed: i.claimed
+            };
+        }));
+
+        const nonClaimedItems = items.filter(i => !i.claimed);
+        setAuctions(nonClaimedItems);
+        console.log(`Loaded ${nonClaimedItems.length} auctions`);
+        console.log(`nonClaimedItems: ${JSON.stringify(nonClaimedItems)}`);
+
+        if (loadingState === "loaded" && !nfts.length) {
+            return (
+                <h1 className="text-center">You have not ongoing auctions</h1>
+            )
+        }
+    }
+
+    async function loadNFTs(marketContract, tokenContract, signer) {
+        // const web3modal = new Web3Modal();
+        // const connection = await web3modal.connect();
+        // const provider = new ethers.providers.Web3Provider(connection);
+        // const signer = provider.getSigner();
+
+        // const marketContract = new ethers.Contract(nftMarketAddress, NFTMarket.abi, signer);
+        // const tokenContract = new ethers.Contract(nftAddress, NFT.abi, signer);
         const data = await marketContract.getMyNFTForSale();
 
         const items = await Promise.all(data.map(async (i) => {
@@ -47,7 +105,6 @@ export default function ListMyAssets() {
         const soldItems = items.filter(i => i.isSold);
         setNfts(items);
         setSoldNfts(soldItems);
-        setLoadingState('loaded');
 
         if (loadingState === "loaded" && !nfts.length) {
             return (
@@ -67,6 +124,30 @@ export default function ListMyAssets() {
                                     <p className="text-2xl text-white">Price {nft.price} Matic</p>
                                 </div>
                             </div>)
+                    )}
+                    {auctions.map((auction, i) => {
+                        let auctionEndDate = ethers.BigNumber.from(auction.auctionEnd).toNumber();
+                        console.log("auctionEndDate", auctionEndDate);
+                        auctionEndDate = new Date(auctionEndDate * 1000);
+                        console.log("auctionEndDate", auctionEndDate);
+                        auctionEndDate = auctionEndDate.getTime();
+                        console.log("auctionEndDate", auctionEndDate);
+                        auctionEndDate = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(auctionEndDate);
+                        return (
+                            <div key={i} className="bg-grey rounded shadow-md p-4">
+                                <img src={auction.image} className="rounded" />
+                                <div className="p-4 bg-black">
+                                    <p className="text text-white">Starting Price: {auction.startingPrice} Matic</p>
+                                    <p className="text text-white">Highest Bid: {auction.highestBid} Matic</p>
+                                </div>
+                                <div className="p-4 bg-red-500">
+                                    {/* here we will show the auction end date as date format */}
+                                    <p className="text text-white">Auction Ends: {auctionEndDate}</p>
+                                    {/* new line as separation */}
+                                    <div className="border-b-2 border-black-500"></div>
+                                    <p className="text text-white">Highest Bid: {auction.highestBid} Matic</p>
+                                </div>
+                            </div>)}
                     )}
                     {
                         Boolean(soldNfts.length) && (
